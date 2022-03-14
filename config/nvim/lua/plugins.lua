9ee32b475569b2c06f8e05aad1189501c6ca7ef2
skippi <jbtcao@gmail.com>
2022-03-14 00:23:00 -0500

    feat(dial): add plugin
    Dial plugin is useful for toggling/increment/decrementing constants.
    Seems useful, will find out over time.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -233,7 +233,30 @@ return require("packer").startup(function(use)
 			vim.g.user_emmet_mode = "nv"
 		end,
 	})
-	use("lukas-reineke/indent-blankline.nvim")
+	use({
+		"monaqa/dial.nvim",
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.constant.alias.bool,
+					augend.date.alias["%H:%M"],
+					augend.date.alias["%Y-%m-%d"],
+					augend.date.alias["%Y/%m/%d"],
+					augend.integer.alias.binary,
+					augend.integer.alias.decimal_int,
+					augend.integer.alias.hex,
+					augend.semver.alias.semver,
+				},
+			})
+			vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+			vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+			vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+			vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+			vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+			vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
+		end,
+	})
 	use("lewis6991/impatient.nvim")
 	use({
 		"nvim-treesitter/nvim-treesitter",
