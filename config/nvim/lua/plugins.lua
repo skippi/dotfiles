b23401b6e74429093695f6513e44dc0bb4e75501
skippi <jbtcao@gmail.com>
2022-09-12 00:15:41 -0500

    simplify jdtls lsp call command
    We only need to call for `jdtls` because of the mason plugin.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -325,21 +325,7 @@ return require("packer").startup(function(use)
 					end
 					local config = {
 						cmd = {
-							"java",
-							"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-							"-Dosgi.bundles.defaultStartLevel=4",
-							"-Declipse.product=org.eclipse.jdt.ls.core.product",
-							"-Dlog.protocol=true",
-							"-Dlog.level=ALL",
-							"-Xms1g",
-							"--add-modules=ALL-SYSTEM",
-							"--add-opens",
-							"java.base/java.util=ALL-UNNAMED",
-							"--add-opens",
-							"java.base/java.lang=ALL-UNNAMED",
-							"-jar",
-							vim.fn.stdpath("data")
-								.. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+							"jdtls",
 							"-configuration",
 							vim.fn.stdpath("data") .. "/mason/packages/jdtls/config_linux",
 							"-data",
