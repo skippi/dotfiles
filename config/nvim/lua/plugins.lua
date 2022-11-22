708013f7880af74ffabe2cdb8014a41b01bc692f
skippi <jbtcao@gmail.com>
2022-11-22 16:10:35 -0600

    add syntax disable on 20+MB files

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -413,7 +413,17 @@ return require("packer").startup(function(use)
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {},
 				autotag = { enable = true },
-				highlight = { enable = true },
+				highlight = {
+					enable = true,
+					disable = function(_, buf)
+						local max_filesize = 20971520 -- 20 MB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+						return false
+					end,
+				},
 				textobjects = {
 					select = {
 						enable = true,
