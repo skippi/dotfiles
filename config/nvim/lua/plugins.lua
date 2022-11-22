9e447bde44cd7d5a098e9e1b52a67b380a0d2057
skippi <jbtcao@gmail.com>
2022-11-22 16:43:38 -0600

    add [m,[[ mappings for treesitter

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -425,6 +425,26 @@ return require("packer").startup(function(use)
 					end,
 				},
 				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
 					select = {
 						enable = true,
 						lookahead = true,
