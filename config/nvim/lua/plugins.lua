e2ad4e590a7c70fac359dcafbf04c0c0441d528d
skippi <jbtcao@gmail.com>
2022-12-02 02:56:12 -0600

    add nvim-autopair on method completion

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -497,8 +497,12 @@ return require("packer").startup(function(use)
 	})
 	use({
 		"windwp/nvim-autopairs",
+		requires = { "hrsh7th/nvim-cmp" },
 		config = function()
 			require("nvim-autopairs").setup({})
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	})
 	use("skippi/vim-abolish")
