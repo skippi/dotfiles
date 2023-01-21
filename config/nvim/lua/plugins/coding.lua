89c6b7aeda39ef00157345862fc5549e2a860d34
skippi <jbtcao@gmail.com>
2023-01-21 16:41:56 -0600

    add mini.ai plugin
    This seems to work nicer than targets vim.

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -3,11 +3,37 @@ return {
 	{ "kana/vim-textobj-entire", dependencies = "kana/vim-textobj-user" },
 	{ "Julian/vim-textobj-variable-segment", dependencies = "kana/vim-textobj-user" },
 	{
-		"wellle/targets.vim",
-		config = function()
-			vim.fn["targets#mappings#extend"]({
-				r = { pair = { { o = "[", c = "]" } } },
-			})
+		"echasnovski/mini.ai",
+		keys = {
+			{ "a", mode = { "x", "o" } },
+			{ "i", mode = { "x", "o" } },
+		},
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- no need to load the plugin, since we only need its queries
+					require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+				end,
+			},
+		},
+		opts = function()
+			local ai = require("mini.ai")
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					r = { { "%b[]" }, "^.().*().$" },
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+					o = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}, {}),
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
 		end,
 	},
 }
