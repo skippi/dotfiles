effa73266eeb877af75a5430d69e4c5c25957f66
skippi <jbtcao@gmail.com>
2023-01-21 16:44:31 -0600

    remove unused treesitter textobjects
    Now a part of mini.ai.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -458,20 +458,6 @@ return {
 							["[]"] = "@class.outer",
 						},
 					},
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["ai"] = "@block.outer",
-							["ii"] = "@block.inner",
-						},
-					},
 				},
 			})
 		end,
