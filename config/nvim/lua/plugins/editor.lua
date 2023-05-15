cdc1d1cc0b523c6192688140f7e39c4d6d5749cd
skippi <jbtcao@gmail.com>
2023-05-14 23:47:18 -0500

    add temporary workaround for telescope insert bug

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -193,6 +193,13 @@ return {
 		},
 		opts = function()
 			local actions = require("telescope.actions")
+			vim.api.nvim_create_autocmd("WinLeave", {
+				callback = function()
+					if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+					end
+				end,
+			})
 			return {
 				defaults = {
 					mappings = {
