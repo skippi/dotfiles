fb02996ac52c146e0a2a00a4ea82ebc1a840a6e5
skippi <jbtcao@gmail.com>
2023-09-20 08:41:47 -0500

    run stylua

--- config/nvim/lua/skippi/util.lua
+++ config/nvim/lua/skippi/util.lua
@@ -46,7 +46,9 @@ function M.edit_file_by_offset(offset)
 	local files = vim.fn.readdir(dir, function(f)
 		return vim.fn.isdirectory(dir .. "/" .. f) == 0
 	end)
-	table.sort(files, function(a, b) return a:lower() < b:lower() end)
+	table.sort(files, function(a, b)
+		return a:lower() < b:lower()
+	end)
 	local idx = -math.huge
 	for i, v in ipairs(files) do
 		if v == vim.fn.expand("%:t") then
