65e4d7d35a7936caf2638a456b65035e02dbec5b
skippi <jbtcao@gmail.com>
2024-04-14 00:55:18 -0500

    map dO/dP to user diff commands
    These work properly in diff mode and normal mode.

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -312,8 +312,8 @@ return {
 		keys = {
 			{ "do", "<Cmd>Diffget<CR>", mode = { "n" } },
 			{ "dp", "<Cmd>Diffput<CR>", mode = { "n" } },
-			{ "dO", "<Cmd>Gitsigns reset_buffer<CR>", mode = { "n" } },
-			{ "dP", "<Cmd>Gitsigns stage_buffer<CR>", mode = { "n" } },
+			{ "dO", "<Cmd>%Diffget<CR>", mode = { "n" } },
+			{ "dP", "<Cmd>%Diffput<CR>", mode = { "n" } },
 			{ "ih", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" }, silent = true },
 			{ "ah", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" }, silent = true },
 		},
