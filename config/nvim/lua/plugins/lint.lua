61bf882babdff5e5676cdc54acd99e72b3e217ac
skippi <jbtcao@gmail.com>
2024-10-16 23:39:37 -0500

    add gitlint to nvim-lint

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -5,6 +5,7 @@ return {
 			local lint = require("lint")
 			lint.linters_by_ft = {
 				cpp = { "cppcheck" },
+				gitcommit = { "gitlint" },
 				python = { "mypy" },
 			}
 			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
